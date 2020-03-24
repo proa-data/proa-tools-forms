0dcc751565ca988be4438ef5d75c38a2419f1128
Miquel Vicens <mvicens@proadata.com>
2020-03-24 16:22:01 +0100

    Fix form autocompletion, setting "name" attribute

--- demo/app.js
+++ demo/app.js
@@ -1,3 +1,11 @@
 ( function() {
-angular.module( 'app', [ 'proaTools.forms' ] );
+angular
+	.module( 'app', [ 'proaTools.forms' ] )
+	.controller( 'DemoController', DemoController );
+
+function DemoController() {
+	var vm = this;
+	vm.isChecked = true;
+	vm.option = 1;
+}
 } )();
