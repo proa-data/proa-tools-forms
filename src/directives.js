57532efd28248c6bda0ea9ef182de0df0139cbc3
Miquel Al. Vicens <mvicens@proadata.com>
2021-03-12 10:49:01 +0100

    Fix transforming DOM inside compile function instead of its post-link

--- src/directives.js
+++ src/directives.js
@@ -11,24 +11,22 @@ function formGroup() {
 		compile: compile
 	};
 
-	function compile() {
-		return function( scope, iElement ) {
-			var input = iElement.find( '.form-control, input[type="file"], .form-check-input' );
-			if ( input.length ) {
-				var id = input.prop( 'id' );
-				if ( !id ) {
-					id = 'form-control-' + n++;
-					input.prop( 'id', id );
-				}
-				iElement.find( 'label' ).prop( 'for', id );
-
-				var name = input.prop( 'name' );
-				if ( !name ) {
-					name = input.attr( 'ng-model' );
-					input.prop( 'name', name ? _.kebabCase( name ) : id );
-				}
+	function compile( tElement ) {
+		var input = tElement.find( '.form-control, input[type="file"], .form-check-input' );
+		if ( input.length ) {
+			var id = input.prop( 'id' );
+			if ( !id ) {
+				id = 'form-control-' + n++;
+				input.prop( 'id', id );
 			}
-		};
+			tElement.find( 'label' ).prop( 'for', id );
+
+			var name = input.prop( 'name' );
+			if ( !name ) {
+				name = input.attr( 'ng-model' );
+				input.prop( 'name', name ? _.kebabCase( name ) : id );
+			}
+		}
 	}
 }
 } )();
